e6175999c7934f31beb78ef01bf61dd35c6ee0bc
Rodrigo Aguayo <roodaguayo@gmail.com>
2021-11-02 18:26:37 -0300

    New version with terra support

---
+++ WEAP_Input.R
@@ -0,0 +1,137 @@
+rm(list=ls())
+cat("\014")  
+
+library("terra")
+library("readxl")
+library("tools")
+
+HRUs<-vect("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Elevation_bands.shp")
+
+#Precipitation baseline
+PP_CR2MET <- rast("E:/Datasets/CR2MET/CR2MET_pr_v2.0_day_1979_2020_005deg.nc")
+PP_CR2MET <- crop(PP_CR2MET, HRUs)[[-1]]
+PP_CR2MET <- tapp(PP_CR2MET, strftime(time(PP_CR2MET),format="%Y-%W"), fun = sum) 
+
+PP_CR2MET_w <- as.data.frame(t(terra::extract(PP_CR2MET, HRUs ,method = "bilinear", fun=mean, na.rm=TRUE)))
+PP_CR2MET_w <- PP_CR2MET_w[-1, ]
+colnames(PP_CR2MET_w) <- HRUs$ID
+write.csv(PP_CR2MET_w, "PP_Input.csv")
+
+#Temperature baseline
+T2M_CR2MET <- rast("E:/Datasets/CR2MET/CR2MET_t2m_v2.0_day_1979_2020_005deg.nc")
+T2M_CR2MET <- crop(T2M_CR2MET, HRUs)[[-1]]
+T2M_CR2MET <- tapp(T2M_CR2MET, strftime(time(T2M_CR2MET),format="%Y-%W"), fun = mean) 
+
+T2M_CR2MET_w <- as.data.frame(t(terra::extract(T2M_CR2MET, HRUs ,method = "bilinear", fun=mean, na.rm=TRUE)))
+T2M_CR2MET_w <- T2M_CR2MET_w[-1, ]
+colnames(T2M_CR2MET_w) <- HRUs$ID
+write.csv(T2M_CR2MET_w, "PP_Input.csv")
+
+
+
+
+
+## OLD
+
+#Wind baseline
+temp_weap<-t(extract(temp,subcuenca2,fun=mean,na.rm=TRUE))
+colnames(temp_weap)<- subcuenca2$Name
+write.xlsx(temp_weap, "Entrada_Wind.xlsx")
+
+#humidity baseline
+temp_weap<-t(extract(temp,subcuenca2,fun=mean,na.rm=TRUE))
+colnames(temp_weap)<- subcuenca2$Name
+write.xlsx(temp_weap, "Entrada_Humedad.xlsx")
+
+#Climate projections: WEAP 
+subcuenca<-shapefile("C:/Users/Rodrigo/Dropbox/Puelo/SIG/Areas/Subcuencas_Puelo2_elep.shp")
+intro<-read.csv2("C:/Users/Rodrigo/Documents/WEAP Areas/Puelo_River_v2/Database/Encabezado.csv", header = FALSE)
+
+pp_base<-stack("C:/Users/Rodrigo/Dropbox/Puelo/Datos/Precipitacion/Datos_Grillados_PP/PP_v3_2000_2019.nc")
+pp_base<-setZ(pp_base,seq(as.Date("2000/1/1"), as.Date("2019/5/1"), "month"))
+pp_base<-pp_base[[which(getZ(pp_base) >= as.Date("2000/4/1") & getZ(pp_base) <= as.Date("2019/3/31"))]]
+pp_lista<-list.files("C:/Users/Rodrigo/Dropbox/Puelo/Proyecciones/Resultados/Precipitacion",pattern = "nc4$", full.names = TRUE)
+
+t2m_base<-stack("C:/Users/Rodrigo/Dropbox/Puelo/Datos/Temperatura/Datos_Grillados_TEMP/Temp_v2_t2m_2000_2019.nc")
+t2m_base<-setZ(t2m_base,seq(as.Date("2000/3/1"), as.Date("2019/5/1"), "month"))
+t2m_base<-t2m_base[[which(getZ(t2m_base) >= as.Date("2000/4/1") & getZ(t2m_base) <= as.Date("2019/3/31"))]]
+t2m_lista<-list.files("C:/Users/Rodrigo/Dropbox/Puelo/Proyecciones/Resultados/Temperatura",pattern = "nc4$", full.names = TRUE)
+
+time<-seq(as.Date('2000/4/1'), as.Date('2070-12-31'), by = 'month')
+año<-format(as.Date(time, format="%d-%m-%Y"),"%Y")
+mes<-rep(c(4,5,6,7,8,9,10,11,12,1,2,3),71)[1:849]
+
+for(j in 1:90) {
+  
+  pp_future<-stack(pp_lista[j])
+  pp_future<-setZ(pp_future,seq(as.Date("2019/1/1"), as.Date("2070/12/1"), "month"))
+  pp_future<-pp_future[[which(getZ(pp_future) >= as.Date("2019/4/1"))]]
+  pp_future<-stack(pp_base,pp_future)
+  pp_future<-disaggregate(pp_future, 5, method='')
+  
+  t2m_future<-stack(t2m_lista[j])
+  t2m_future<-setZ(t2m_future,seq(as.Date("2019/1/1"), as.Date("2070/12/1"), "month"))
+  t2m_future<-t2m_future[[which(getZ(t2m_future) >= as.Date("2019/4/1"))]]
+  t2m_future<-stack(t2m_base,t2m_future)
+  t2m_future<-disaggregate(t2m_future, 5, method='')
+  
+  pp_weap<-round(t(extract(pp_future,subcuenca,fun=mean,na.rm=TRUE)),0)
+  t2m_weap<-round(t(extract(t2m_future,subcuenca,fun=mean,na.rm=TRUE)),2)
+  
+  pp_weap<-cbind(mes, pp_weap)
+  pp_weap<-cbind(a?o, pp_weap)
+  colnames(pp_weap)<- colnames(intro)
+  pp_weap<-rbind(intro,pp_weap)
+  
+  t2m_weap<-cbind(mes, t2m_weap)
+  t2m_weap<-cbind(a?o, t2m_weap)
+  colnames(t2m_weap)<- colnames(intro)
+  t2m_weap<-rbind(intro,t2m_weap)
+  
+  write.table(t2m_weap,paste0(file_path_sans_ext(basename(t2m_lista[j])),".csv"), sep=";", row.names = FALSE, col.names = FALSE, quote = FALSE)
+  write.table(pp_weap,paste0(file_path_sans_ext(basename(pp_lista[j])),".csv"), sep=";", row.names = FALSE, col.names = FALSE, quote = FALSE)
+  print(j)
+}
+
+#Climate projections: Puelo mean
+cuenca<-shapefile("C:/Users/Rodrigo/Dropbox/Puelo/SIG/Areas/Cuenca_Puelo.shp")
+cuenca <- spTransform(cuenca, crs("+proj=longlat +datum=WGS84"))
+
+pp_base<-stack("C:/Users/Rodrigo/Dropbox/Puelo/Datos/Precipitacion/Datos_Grillados_PP/PP_v3_2000_2019.nc")
+pp_base<-setZ(pp_base,seq(as.Date("2000/1/1"), as.Date("2019/5/1"), "month"))
+pp_base<-pp_base[[which(getZ(pp_base) >= as.Date("2000/4/1") & getZ(pp_base) <= as.Date("2019/3/31"))]]
+pp_lista<-list.files("C:/Users/Rodrigo/Dropbox/Puelo/Proyecciones/Resultados/Precipitacion",pattern = "nc4$", full.names = TRUE)
+
+t2m_base<-stack("C:/Users/Rodrigo/Dropbox/Puelo/Datos/Temperatura/Datos_Grillados_TEMP/Temp_v2_t2m_2000_2019.nc")
+t2m_base<-setZ(t2m_base,seq(as.Date("2000/3/1"), as.Date("2019/5/1"), "month"))
+t2m_base<-t2m_base[[which(getZ(t2m_base) >= as.Date("2000/4/1") & getZ(t2m_base) <= as.Date("2019/3/31"))]]
+t2m_lista<-list.files("C:/Users/Rodrigo/Dropbox/Puelo/Proyecciones/Resultados/Temperatura",pattern = "nc4$", full.names = TRUE)
+
+pp_results<-matrix(0,90,849)
+t2m_results<-matrix(0,90,849)
+rownames(pp_results)<-basename(pp_lista)
+rownames(t2m_results)<-basename(t2m_lista)
+
+for(j in 1:90) {
+  pp_future<-stack(pp_lista[j])
+  pp_future<-setZ(pp_future,seq(as.Date("2019/1/1"), as.Date("2070/12/1"), "month"))
+  pp_future<-pp_future[[which(getZ(pp_future) >= as.Date("2019/4/1"))]]
+  pp_future<-stack(pp_base,pp_future)
+
+  t2m_future<-stack(t2m_lista[j])
+  t2m_future<-setZ(t2m_future,seq(as.Date("2019/1/1"), as.Date("2070/12/1"), "month"))
+  t2m_future<-t2m_future[[which(getZ(t2m_future) >= as.Date("2019/4/1"))]]
+  t2m_future<-stack(t2m_base,t2m_future)
+
+  pp_results[j,]<-round(t(extract(pp_future,cuenca,fun=mean,na.rm=TRUE)),0)
+  t2m_results[j,]<-round(t(extract(t2m_future,cuenca,fun=mean,na.rm=TRUE)),2)
+  print(j)
+}
+
+write.xlsx(pp_results, "pp_2040_2070.xlsx")
+write.xlsx(t2m_results, "t2m_2040_2070.xlsx")
+
+  
+  
+
+
