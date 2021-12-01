19d301adeb958728107970f6c112969bb3e9802e
Rodrigo Aguayo <roodaguayo@gmail.com>
2021-12-01 10:23:15 -0300

    Added ET, PET and CC

--- Climate_to_daily.R
+++ Climate_to_daily.R
@@ -8,9 +8,10 @@ HRUs<-ext(c(-73.6,-70.8,-49.4,-41.1))
 
 ## Precipitation baseline
 PP_CR2MET <- rast("E:/Datasets/CR2MET/CR2MET_pr_v2.0_day_1979_2020_005deg.nc",  subds="pr")
-PP_CR2MET <- crop(PP_CR2MET,HRUs)
 PP_CR2MET <- subset(PP_CR2MET, which(time(PP_CR2MET) > '1979-03-31' & (time(PP_CR2MET) <= '2020-03-31')))
-PP_CR2MET_m <- tapp(PP_CR2MET, strftime(time(PP_CR2MET),format="%Y"), fun = sum)
+PP_CR2MET <- crop(PP_CR2MET,HRUs)
+
+PP_CR2MET_m <- tapp(PP_CR2MET, strftime(time(PP_CR2MET),format="%Y-%W"), fun = sum)
 PP_CR2MET_m <- mean(PP_CR2MET_m[[-c(1,nlyr(PP_CR2MET_m))]]) #Exclude first and last
 
 writeRaster(PP_CR2MET_m, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Precipitation/PP_CR2MET_mean.tif", datatype="INT4S", overwrite = TRUE)
@@ -19,14 +20,24 @@ writeCDF(PP_CR2MET, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Precipitat
 
 
 ## Temperature baseline
-T2M_CR2MET <- rast("E:/Datasets/CR2MET/CR2MET_t2m_v2.0_day_1979_2020_005deg.nc", subds="t2m")
-T2M_CR2MET <- crop(T2M_CR2MET, HRUs)
-T2M_CR2MET <- subset(T2M_CR2MET, which(time(T2M_CR2MET) > '1979-03-31' & (time(T2M_CR2MET) <= '2020-03-31')))
-T2M_CR2MET_m <- tapp(T2M_CR2MET, strftime(time(T2M_CR2MET),format="%Y"), fun = mean)
-T2M_CR2MET_m <- mean(T2M_CR2MET_m[[-c(1,nlyr(T2M_CR2MET_m))]]) #Exclude first and last
+T2M_CR2MET_v0 <- rast("E:/Datasets/CR2MET/CR2MET_t2m_v2.0_day_1979_2020_005deg.nc", subds="t2m")
+T2M_CR2MET_v0 <- subset(T2M_CR2MET_v0, which(time(T2M_CR2MET_v0) > '1979-03-31' & (time(T2M_CR2MET_v0) <= '2020-03-31')))
+T2M_CR2MET_v0 <- crop(T2M_CR2MET_v0, HRUs) # Crop for study area
+T2M_CR2MET_v1 <- disagg(T2M_CR2MET_v0, 2)
 
-writeRaster(T2M_CR2MET_m, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Temperature/T2M_CR2MET_mean.tif", overwrite = TRUE)
-writeCDF(T2M_CR2MET, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Temperature/T2M_CR2MET_d.nc",
+dem_001        <- rast("C:/Users/rooda/Dropbox/Patagonia/GIS South/dem_patagonia3f.tif")
+dem_001[is.na(dem_001)] <- 0 # Downscaling from 0.05 to 0.025 (0.01 in variables, ups)
+
+dem_001<-resample(dem_001, T2M_CR2MET_v1, method="bilinear")
+dem_005<-resample(dem_001, T2M_CR2MET_v0, method="bilinear")
+dem_005<-resample(dem_005, dem_001, method="near")
+T2M_CR2MET_v1<-T2M_CR2MET_v1+(dem_005-dem_001)*0.0065
+
+T2M_CR2MET_v1m <- tapp(T2M_CR2MET_v1, strftime(time(T2M_CR2MET_v1),format="%Y"), fun = mean)
+T2M_CR2MET_v1m <- mean(T2M_CR2MET_v1m[[-c(1,nlyr(T2M_CR2MET_v1m))]]) #Exclude first and last
+
+writeRaster(T2M_CR2MET_v1m, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Temperature/T2M_CR2MET_mean.tif", overwrite = TRUE)
+writeCDF(T2M_CR2MET_v1, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Temperature/T2M_CR2MET_d.nc",
          overwrite=TRUE, varname="t2m", unit="degC", longname="temperature", zname="time", compression = 9)
 
 
@@ -114,11 +125,57 @@ writeCDF(RH_ERA5L_d, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Others/RH
 
 
 ## Cloud cover
-CC_MODIS <- rast(list.files("E:/Datasets/CLOUD_COVER/", full.names = T))
-CC_MODIS <- crop(CC_MODIS, HRUs)*0.01
-CC_MODIS[CC_MODIS > 100] <- NA
+CC_MODIS   <- rast(list.files("E:/Datasets/CLOUD_COVER2/", full.names = T, recursive = FALSE, pattern = ".tif$"))
+time_modis <- MODIS::extractDate(list.files("E:/Datasets/CLOUD_COVER2/HDF", recursive = FALSE), asDate = TRUE)
+CC_MODIS <- crop(CC_MODIS, HRUs)
+time(CC_MODIS)<-time_modis$inputLayerDates
 
-writeRaster(mean(CC_MODIS), "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Others/CC_MODIS_mean.tif", datatype="INT4S", overwrite = TRUE)
+CC_MODIS_m <- tapp(CC_MODIS, strftime(time(CC_MODIS),format="%Y"), fun = mean, na.rm = TRUE)
+CC_MODIS_m <- mean(CC_MODIS_m[[-c(1,nlyr(CC_MODIS_m))]])
+
+writeRaster(CC_MODIS_m, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Others/CC_MODIS_mean.tif", datatype="INT4S", overwrite = TRUE)
 writeCDF(CC_MODIS, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Others/CC_MODIS.nc", overwrite=TRUE, 
          varname="cc", unit="%", longname="Cloud cover", zname="time", compression = 9)
+
+
+## Evapotranspiration (ET) and potential evapotranspiration (PET)
+library("raster")
+PET_GLEAM <- stack(list.files("E:/Datasets/GLEAM/EP", full.names = T), varname="Ep")
+crs(PET_GLEAM) <- crs(shapefile("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Elevation_bands.shp") )
+PET_GLEAM <- setZ(PET_GLEAM, seq(as.Date("1980/1/1"), as.Date("2020/12/31"), "day"))
+
+ET_GLEAM <- stack(list.files("E:/Datasets/GLEAM/E", full.names = T), varname="E")
+crs(ET_GLEAM) <- crs(shapefile("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Elevation_bands.shp") )
+ET_GLEAM <- setZ(ET_GLEAM, seq(as.Date("1980/1/1"), as.Date("2020/12/31"), "day"))
+
+for (i in 1980:2020){
+  PET_GLEAM_i <- subset(PET_GLEAM, which(getZ(PET_GLEAM) >= as.Date(paste0(i, "-01-01")) & (getZ(PET_GLEAM) <= as.Date(paste0(i, "-12-31")))))
+  PET_GLEAM_i <- flip(t(PET_GLEAM_i), 1)
+  PET_GLEAM_i <- crop(PET_GLEAM_i, extent(c(-75,-70,-51,-40)))
+  writeRaster(PET_GLEAM_i, paste0("PET_GLEAM_",i,".nc"), format = "CDF", overwrite=TRUE, varname="pet", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time(day)")
+  
+  ET_GLEAM_i <- subset(ET_GLEAM, which(getZ(ET_GLEAM) >= as.Date(paste0(i, "-01-01")) & (getZ(ET_GLEAM) <= as.Date(paste0(i, "-12-31")))))
+  ET_GLEAM_i <- flip(t(ET_GLEAM_i), 1)
+  ET_GLEAM_i <- crop(ET_GLEAM_i, extent(c(-75,-70,-51,-40)))
+  writeRaster(ET_GLEAM_i, paste0("ET_GLEAM_",i,".nc"), format = "CDF", overwrite=TRUE, varname="et", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time(day)")
+  print(i)
+}
+
+ET_GLEAM   <- rast(list.files("E:/Datasets/GLEAM/E_v2", full.names = T, recursive = FALSE, pattern = ".nc$"))
+time(ET_GLEAM) <- seq(as.Date("1980/1/1"), as.Date("2020/12/31"), "day")
+ET_GLEAM_m <- tapp(ET_GLEAM, strftime(time(ET_GLEAM),format="%Y"), fun = sum, na.rm = TRUE)
+ET_GLEAM_m <- mean(ET_GLEAM_m[[-c(1,nlyr(ET_GLEAM_m))]])
+
+writeRaster(ET_GLEAM_m, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Evapotranspiration/ET_GLEAM_mean.tif", overwrite = TRUE)
+writeCDF(ET_GLEAM, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Evapotranspiration/ET_GLEAM.nc", overwrite=TRUE, 
+         varname="et", unit="mm", longname="Evapotranspiration", zname="time", compression = 9)
+
+PET_GLEAM   <- rast(list.files("E:/Datasets/GLEAM/EP_v2", full.names = T, recursive = FALSE, pattern = ".nc$"))
+time(PET_GLEAM) <- seq(as.Date("1980/1/1"), as.Date("2020/12/31"), "day")
+PET_GLEAM_m <- tapp(PET_GLEAM, strftime(time(PET_GLEAM),format="%Y"), fun = sum, na.rm = TRUE)
+PET_GLEAM_m <- mean(PET_GLEAM_m[[-c(1,nlyr(PET_GLEAM_m))]])
+
+writeRaster(PET_GLEAM_m, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Evapotranspiration/PET_GLEAM_mean.tif", overwrite = TRUE)
+writeCDF(PET_GLEAM, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Evapotranspiration/PET_GLEAM.nc", overwrite=TRUE, 
+         varname="pet", unit="mm", longname="Potential Evapotranspiration", zname="time", compression = 9)
 
