cbe7684e1dfc89f3d748d617e5397b8629068235
Rodrigo Aguayo <roodaguayo@gmail.com>
2022-04-19 15:12:33 -0400

    new coastal basins

--- WEAP_Input.R
+++ WEAP_Input.R
@@ -7,15 +7,50 @@ library("sf")
 
 #Choose one
 HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Elevation_bands.shp") #Elevation bands
-HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuenca_Yelcho.shp") #Elevation bands
-HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuenca_Pascua.shp") #Elevation bands
-HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuenca_Aysen.shp") #Elevation bands
-HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuenca_Baker.shp") #Elevation bands
+date<-seq(from = as.POSIXct("1979-04-01", tz="UTC"), to = as.POSIXct("2020-03-31", tz="UTC"), by = "day")
+name<-"Puelo"
+
+HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuencas/Cuenca_Yelcho.shp") #Elevation bands
+date<-seq(from = as.POSIXct("1979-04-01", tz="UTC"), to = as.POSIXct("2020-03-31", tz="UTC"), by = "day")
+name<-"Yelcho"
+
+HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuencas/Cuenca_Pascua.shp") #Elevation bands
+date<-seq(from = as.POSIXct("1979-04-01", tz="UTC"), to = as.POSIXct("2020-03-31", tz="UTC"), by = "day")
+name<-"Pascua"
+
+HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuencas/Cuenca_Aysen.shp") #Elevation bands
+date<-seq(from = as.POSIXct("1979-04-01", tz="UTC"), to = as.POSIXct("2020-03-31", tz="UTC"), by = "day")
+name<-"Aysen"
+
+HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuencas/Cuenca_Baker.shp") #Elevation bands
 date<-seq(from = as.POSIXct("1979-04-01", tz="UTC"), to = as.POSIXct("2020-03-31", tz="UTC"), by = "day")
 name<-"Baker"
 
+HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuencas/Cuenca_Pcostera.shp") #Elevation bands
+date<-seq(from = as.POSIXct("1979-04-01", tz="UTC"), to = as.POSIXct("2020-03-31", tz="UTC"), by = "day")
+name<-"P_costeras"
+
+HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuencas/Cuenca_Acostera.shp") #Elevation bands
+date<-seq(from = as.POSIXct("1979-04-01", tz="UTC"), to = as.POSIXct("2020-03-31", tz="UTC"), by = "day")
+name<-"A_costeras"
+
+HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuencas/Cuenca_Bcostera.shp") #Elevation bands
+date<-seq(from = as.POSIXct("1979-04-01", tz="UTC"), to = as.POSIXct("2020-03-31", tz="UTC"), by = "day")
+name<-"B_costeras"
+
+HRUs<-st_read("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Cuencas/Cuenca_David.shp") #Elevation bands
+date2<-seq(from = as.POSIXct("1979-01-01", tz="UTC"), to = as.POSIXct("2020-04-30", tz="UTC"), by = "day")
+name<-"David"
+
 ## Precipitation baseline
+PP_CR2MET_d <- rast("E:/Datasets/CR2MET/CR2MET_pr_v2.0_day_1979_2020_005deg.nc",  subds="pr") # For basins in Central Chile
 PP_CR2MET_d <- rast("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Precipitation/PP_CR2MET_d.nc")
+factor<-rast("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Precipitation/PP_factor.tif")
+
+factor <- crop(factor, project(vect(HRUs), factor))
+PP_CR2MET_d <- crop(PP_CR2MET_d,factor)
+PP_CR2MET_d<-PP_CR2MET_d*factor
+
 PP_CR2MET_d <- as.data.frame(t(exact_extract(PP_CR2MET_d, HRUs , 'mean')))
 PP_CR2MET_d <- round(PP_CR2MET_d,0)
 colnames(PP_CR2MET_d) <- HRUs$ID_WEAP
@@ -23,13 +58,30 @@ PP_CR2MET_d <- cbind(date,PP_CR2MET_d)
 write.csv(PP_CR2MET_d, paste0("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/WEAP/Inputs/Input_PP_",name,".csv"), row.names = FALSE)
 
 
-## Temperature baseline
+## Temperature baseline (mean)
+T2M_CR2MET_d <- rast("E:/Datasets/CR2MET/CR2MET_t2m_v2.0_day_1979_2020_005deg.nc", subds="t2m") # For basins in Central Chile
 T2M_CR2MET_d <- rast("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Temperature/T2M_CR2MET_d.nc")
 T2M_CR2MET_d <- as.data.frame(t(exact_extract(T2M_CR2MET_d, HRUs , "mean")))
 T2M_CR2MET_d <- round(T2M_CR2MET_d,2)
 colnames(T2M_CR2MET_d) <- HRUs$ID_WEAP
 T2M_CR2MET_d <- cbind(date, T2M_CR2MET_d)
 write.csv(T2M_CR2MET_d, paste0("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/WEAP/Inputs/Input_T2M_",name,".csv"), row.names = FALSE)
+
+
+## Temperature baseline (max and min)
+T2M_CR2MET_d <- rast("E:/Datasets/CR2MET/CR2MET_tmax_v2.0_day_1979_2020_005deg.nc", subds="tmax") # For basins in Central Chile
+T2M_CR2MET_d <- as.data.frame(t(exact_extract(T2M_CR2MET_d, HRUs , "mean")))
+T2M_CR2MET_d <- round(T2M_CR2MET_d,2)
+colnames(T2M_CR2MET_d) <- HRUs$ID_WEAP
+T2M_CR2MET_d <- cbind(date2, T2M_CR2MET_d)
+write.csv(T2M_CR2MET_d, paste0("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/WEAP/Inputs/Input_T2Mmax_",name,".csv"), row.names = FALSE)
+
+T2M_CR2MET_d <- rast("E:/Datasets/CR2MET/CR2MET_tmin_v2.0_day_1979_2020_005deg.nc", subds="tmin") # For basins in Central Chile
+T2M_CR2MET_d <- as.data.frame(t(exact_extract(T2M_CR2MET_d, HRUs , "mean")))
+T2M_CR2MET_d <- round(T2M_CR2MET_d,2)
+colnames(T2M_CR2MET_d) <- HRUs$ID_WEAP
+T2M_CR2MET_d <- cbind(date2, T2M_CR2MET_d)
+write.csv(T2M_CR2MET_d, paste0("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/WEAP/Inputs/Input_T2Mmin_",name,".csv"), row.names = FALSE)
 
 
 ## Wind baseline
@@ -75,7 +127,7 @@ PET_ERA5L_d <- rast("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Evapotrans
 PET_ERA5L_d <- as.data.frame(t(exact_extract(PET_ERA5L_d, HRUs, "mean")))
 PET_ERA5L_d <- round(PET_ERA5L_d,1)
 colnames(PET_ERA5L_d) <- HRUs$ID_WEAP
-PET_ERA5L_d <- cbind(date, PET_ERA5L_d)
+PET_ERA5L_d <- cbind(seq(as.Date("1980/1/1"), as.Date("2020/12/31"), "day"), PET_ERA5L_d)
 write.csv(PET_ERA5L_d, paste0("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/WEAP/Inputs/Input_PET_",name,".csv"), row.names = FALSE)
 
 #Evapotranspiration (ET)
@@ -83,15 +135,12 @@ ET_ERA5L_d <- rast("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Evapotransp
 ET_ERA5L_d <- as.data.frame(t(exact_extract(ET_ERA5L_d, HRUs, "mean")))
 ET_ERA5L_d <- round(ET_ERA5L_d,1)
 colnames(ET_ERA5L_d) <- HRUs$ID_WEAP
-ET_ERA5L_d <- cbind(date, ET_ERA5L_d)
+ET_ERA5L_d <- cbind(seq(as.Date("1980/1/1"), as.Date("2020/12/31"), "day"), ET_ERA5L_d)
 write.csv(ET_ERA5L_d, paste0("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/WEAP/Inputs/Input_ET_",name,".csv"), row.names = FALSE)
 
 
 
 
-## Climate projections
-
-# TODO  ####
 
 
 
