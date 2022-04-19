e7f400b9da99097afae1d4c563782ea4f3d8a5d9
Rodrigo Aguayo <roodaguayo@gmail.com>
2022-04-19 15:14:14 -0400

    small corrections

--- Climate_to_daily.R
+++ Climate_to_daily.R
@@ -4,14 +4,20 @@ cat("\014")
 library("terra")
 
 HRUs<-vect("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/GIS/Elevation_bands.shp") #Default for Puelo 
-HRUs<-ext(c(-73.6,-70.8,-49.4,-41.1))
+HRUs<-ext(c(-73.9,-70.8,-49.4,-40.7))
+
+HRUs<-ext(c(-74,-70,-38,-31)) #For RH and wind speed (David)
 
 ## Precipitation baseline
 PP_CR2MET <- rast("E:/Datasets/CR2MET/CR2MET_pr_v2.0_day_1979_2020_005deg.nc",  subds="pr")
 PP_CR2MET <- subset(PP_CR2MET, which(time(PP_CR2MET) > '1979-03-31' & (time(PP_CR2MET) <= '2020-03-31')))
 PP_CR2MET <- crop(PP_CR2MET,HRUs)
 
-PP_CR2MET_m <- tapp(PP_CR2MET, strftime(time(PP_CR2MET),format="%Y-%W"), fun = sum)
+factor<-rast("C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Precipitation/PP_factor.tif")
+factor <- crop(factor, PP_CR2MET)
+PP_CR2MET_d<-PP_CR2MET*factor
+
+PP_CR2MET_m <- tapp(PP_CR2MET, strftime(time(PP_CR2MET),format="%Y"), fun = sum)
 PP_CR2MET_m <- mean(PP_CR2MET_m[[-c(1,nlyr(PP_CR2MET_m))]]) #Exclude first and last
 
 writeRaster(PP_CR2MET_m, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Precipitation/PP_CR2MET_mean.tif", datatype="INT4S", overwrite = TRUE)
@@ -42,23 +48,23 @@ writeCDF(T2M_CR2MET_v1, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Temper
 
 
 ## Wind speed baseline
-WD_ERA5L_u00 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_00hrs.nc", subds="u10"), HRUs)
-WD_ERA5L_u03 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_03hrs.nc", subds="u10"), HRUs)
-WD_ERA5L_u06 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_06hrs.nc", subds="u10"), HRUs)
-WD_ERA5L_u09 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_09hrs.nc", subds="u10"), HRUs)
-WD_ERA5L_u12 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_12hrs.nc", subds="u10"), HRUs)
-WD_ERA5L_u15 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_15hrs.nc", subds="u10"), HRUs)
-WD_ERA5L_u18 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_18hrs.nc", subds="u10"), HRUs)
-WD_ERA5L_u21 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_21hrs.nc", subds="u10"), HRUs)
+WD_ERA5L_u00 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_00hrs.nc", subds="u10"), HRUs)
+WD_ERA5L_u03 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_03hrs.nc", subds="u10"), HRUs)
+WD_ERA5L_u06 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_06hrs.nc", subds="u10"), HRUs)
+WD_ERA5L_u09 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_09hrs.nc", subds="u10"), HRUs)
+WD_ERA5L_u12 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_12hrs.nc", subds="u10"), HRUs)
+WD_ERA5L_u15 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_15hrs.nc", subds="u10"), HRUs)
+WD_ERA5L_u18 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_18hrs.nc", subds="u10"), HRUs)
+WD_ERA5L_u21 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_21hrs.nc", subds="u10"), HRUs)
 
-WD_ERA5L_v00 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_00hrs.nc", subds="v10"), HRUs)
-WD_ERA5L_v03 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_03hrs.nc", subds="v10"), HRUs)
-WD_ERA5L_v06 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_06hrs.nc", subds="v10"), HRUs)
-WD_ERA5L_v09 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_09hrs.nc", subds="v10"), HRUs)
-WD_ERA5L_v12 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_12hrs.nc", subds="v10"), HRUs)
-WD_ERA5L_v15 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_15hrs.nc", subds="v10"), HRUs)
-WD_ERA5L_v18 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_18hrs.nc", subds="v10"), HRUs)
-WD_ERA5L_v21 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_21hrs.nc", subds="v10"), HRUs)
+WD_ERA5L_v00 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_00hrs.nc", subds="v10"), HRUs)
+WD_ERA5L_v03 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_03hrs.nc", subds="v10"), HRUs)
+WD_ERA5L_v06 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_06hrs.nc", subds="v10"), HRUs)
+WD_ERA5L_v09 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_09hrs.nc", subds="v10"), HRUs)
+WD_ERA5L_v12 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_12hrs.nc", subds="v10"), HRUs)
+WD_ERA5L_v15 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_15hrs.nc", subds="v10"), HRUs)
+WD_ERA5L_v18 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_18hrs.nc", subds="v10"), HRUs)
+WD_ERA5L_v21 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_21hrs.nc", subds="v10"), HRUs)
 
 factor      <- (2/10)^0.25 #Correction from 10m to 2m
 WD_ERA5L_00 <- sqrt(WD_ERA5L_u00^2+WD_ERA5L_v00^2)*factor
@@ -72,7 +78,7 @@ WD_ERA5L_21 <- sqrt(WD_ERA5L_u00^2+WD_ERA5L_v00^2)*factor
 
 WD_ERA5L <- c(WD_ERA5L_00, WD_ERA5L_03, WD_ERA5L_06, WD_ERA5L_09, WD_ERA5L_12, WD_ERA5L_15, WD_ERA5L_18, WD_ERA5L_21)
 WD_ERA5L_d <- tapp(WD_ERA5L, strftime(time(WD_ERA5L),format="%Y-%m-%d"), fun = mean)
-time(WD_ERA5L_d) <- seq(from = as.POSIXct("1979-01-01", tz="UTC"), to = as.POSIXct("2021-09-01", tz="UTC"), by = "day")
+time(WD_ERA5L_d) <- seq(from = as.POSIXct("1979-01-01", tz="UTC"), to = as.POSIXct("2020-12-31", tz="UTC"), by = "day")
 WD_ERA5L_d <- subset(WD_ERA5L_d, which(time(WD_ERA5L_d) > '1979-03-31' & (time(WD_ERA5L_d) <= '2020-03-31')))
 
 WD_ERA5L_m <- tapp(WD_ERA5L_d, strftime(time(WD_ERA5L_d),format="%Y"), fun = mean)
@@ -83,23 +89,23 @@ writeCDF(WD_ERA5L_d, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Others/WD
          overwrite=TRUE, varname="wd", unit="m/s", longname="Wind speed", zname="time", compression = 9)
 
 ## Relative humidity baseline
-T2M_ERA5L_00 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_00hrs.nc", subds="t2m"), HRUs)-273.15
-T2M_ERA5L_03 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_03hrs.nc", subds="t2m"), HRUs)-273.15
-T2M_ERA5L_06 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_06hrs.nc", subds="t2m"), HRUs)-273.15
-T2M_ERA5L_09 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_09hrs.nc", subds="t2m"), HRUs)-273.15
-T2M_ERA5L_12 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_12hrs.nc", subds="t2m"), HRUs)-273.15
-T2M_ERA5L_15 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_15hrs.nc", subds="t2m"), HRUs)-273.15
-T2M_ERA5L_18 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_18hrs.nc", subds="t2m"), HRUs)-273.15
-T2M_ERA5L_21 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_21hrs.nc", subds="t2m"), HRUs)-273.15
+T2M_ERA5L_00 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_00hrs.nc", subds="t2m"), HRUs)-273.15
+T2M_ERA5L_03 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_03hrs.nc", subds="t2m"), HRUs)-273.15
+T2M_ERA5L_06 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_06hrs.nc", subds="t2m"), HRUs)-273.15
+T2M_ERA5L_09 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_09hrs.nc", subds="t2m"), HRUs)-273.15
+T2M_ERA5L_12 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_12hrs.nc", subds="t2m"), HRUs)-273.15
+T2M_ERA5L_15 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_15hrs.nc", subds="t2m"), HRUs)-273.15
+T2M_ERA5L_18 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_18hrs.nc", subds="t2m"), HRUs)-273.15
+T2M_ERA5L_21 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_21hrs.nc", subds="t2m"), HRUs)-273.15
 
-T2Mr_ERA5L_00 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_00hrs.nc", subds="d2m"), HRUs)-273.15
-T2Mr_ERA5L_03 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_03hrs.nc", subds="d2m"), HRUs)-273.15
-T2Mr_ERA5L_06 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_06hrs.nc", subds="d2m"), HRUs)-273.15
-T2Mr_ERA5L_09 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_09hrs.nc", subds="d2m"), HRUs)-273.15
-T2Mr_ERA5L_12 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_12hrs.nc", subds="d2m"), HRUs)-273.15
-T2Mr_ERA5L_15 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_15hrs.nc", subds="d2m"), HRUs)-273.15
-T2Mr_ERA5L_18 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_18hrs.nc", subds="d2m"), HRUs)-273.15
-T2Mr_ERA5L_21 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_40-60lat_21hrs.nc", subds="d2m"), HRUs)-273.15
+T2Mr_ERA5L_00 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_00hrs.nc", subds="d2m"), HRUs)-273.15
+T2Mr_ERA5L_03 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_03hrs.nc", subds="d2m"), HRUs)-273.15
+T2Mr_ERA5L_06 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_06hrs.nc", subds="d2m"), HRUs)-273.15
+T2Mr_ERA5L_09 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_09hrs.nc", subds="d2m"), HRUs)-273.15
+T2Mr_ERA5L_12 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_12hrs.nc", subds="d2m"), HRUs)-273.15
+T2Mr_ERA5L_15 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_15hrs.nc", subds="d2m"), HRUs)-273.15
+T2Mr_ERA5L_18 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_18hrs.nc", subds="d2m"), HRUs)-273.15
+T2Mr_ERA5L_21 <- crop(rast("E:/Datasets/ERA5_LAND/RH_Wind_30-50lat_21hrs.nc", subds="d2m"), HRUs)-273.15
 
 ## Calculated from the partial pressures of water vapor and saturation.
 HR_ERA5L_00 <- exp((17.625*T2Mr_ERA5L_00)/(243.04+T2Mr_ERA5L_00)) / exp((17.625*T2M_ERA5L_00)/(243.04+T2M_ERA5L_00))
@@ -113,7 +119,7 @@ RH_ERA5L_21 <- exp((17.625*T2Mr_ERA5L_21)/(243.04+T2Mr_ERA5L_21)) / exp((17.625*
 
 RH_ERA5L   <- c(HR_ERA5L_00, RH_ERA5L_03, RH_ERA5L_06, RH_ERA5L_09, RH_ERA5L_12, RH_ERA5L_15, RH_ERA5L_18, RH_ERA5L_21)
 RH_ERA5L_d <- tapp(RH_ERA5L, strftime(time(RH_ERA5L),format="%Y-%m-%d"), fun = mean)*100
-time(RH_ERA5L_d) <- seq(from = as.POSIXct("1979-01-01", tz="UTC"), to = as.POSIXct("2021-09-01", tz="UTC"), by = "day")
+time(RH_ERA5L_d) <- seq(from = as.POSIXct("1978-12-31", tz="UTC"), to = as.POSIXct("2020-12-31", tz="UTC"), by = "day")
 RH_ERA5L_d <- subset(RH_ERA5L_d, which(time(RH_ERA5L_d) > '1979-03-31' & (time(RH_ERA5L_d) <= '2020-03-31')))
 
 RH_ERA5L_m <- tapp(RH_ERA5L_d, strftime(time(RH_ERA5L_d),format="%Y"), fun = mean)
@@ -151,18 +157,19 @@ ET_GLEAM <- setZ(ET_GLEAM, seq(as.Date("1980/1/1"), as.Date("2020/12/31"), "day"
 for (i in 1980:2020){
   PET_GLEAM_i <- subset(PET_GLEAM, which(getZ(PET_GLEAM) >= as.Date(paste0(i, "-01-01")) & (getZ(PET_GLEAM) <= as.Date(paste0(i, "-12-31")))))
   PET_GLEAM_i <- flip(t(PET_GLEAM_i), 1)
-  PET_GLEAM_i <- crop(PET_GLEAM_i, extent(c(-75,-70,-51,-40)))
+  PET_GLEAM_i <- crop(PET_GLEAM_i, extent(c(-75,-70,-51,-30)))
   writeRaster(PET_GLEAM_i, paste0("PET_GLEAM_",i,".nc"), format = "CDF", overwrite=TRUE, varname="pet", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time(day)")
   
   ET_GLEAM_i <- subset(ET_GLEAM, which(getZ(ET_GLEAM) >= as.Date(paste0(i, "-01-01")) & (getZ(ET_GLEAM) <= as.Date(paste0(i, "-12-31")))))
   ET_GLEAM_i <- flip(t(ET_GLEAM_i), 1)
-  ET_GLEAM_i <- crop(ET_GLEAM_i, extent(c(-75,-70,-51,-40)))
+  ET_GLEAM_i <- crop(ET_GLEAM_i, extent(c(-75,-70,-51,-30)))
   writeRaster(ET_GLEAM_i, paste0("ET_GLEAM_",i,".nc"), format = "CDF", overwrite=TRUE, varname="et", varunit="mm", xname="Longitude",   yname="Latitude", zname="Time(day)")
   print(i)
 }
 
 ET_GLEAM   <- rast(list.files("E:/Datasets/GLEAM/E_v2", full.names = T, recursive = FALSE, pattern = ".nc$"))
 time(ET_GLEAM) <- seq(as.Date("1980/1/1"), as.Date("2020/12/31"), "day")
+ET_GLEAM <- crop(ET_GLEAM, HRUs)
 ET_GLEAM_m <- tapp(ET_GLEAM, strftime(time(ET_GLEAM),format="%Y"), fun = sum, na.rm = TRUE)
 ET_GLEAM_m <- mean(ET_GLEAM_m[[-c(1,nlyr(ET_GLEAM_m))]])
 
@@ -172,6 +179,7 @@ writeCDF(ET_GLEAM, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Data/Evapotransp
 
 PET_GLEAM   <- rast(list.files("E:/Datasets/GLEAM/EP_v2", full.names = T, recursive = FALSE, pattern = ".nc$"))
 time(PET_GLEAM) <- seq(as.Date("1980/1/1"), as.Date("2020/12/31"), "day")
+PET_GLEAM <- crop(PET_GLEAM, HRUs)
 PET_GLEAM_m <- tapp(PET_GLEAM, strftime(time(PET_GLEAM),format="%Y"), fun = sum, na.rm = TRUE)
 PET_GLEAM_m <- mean(PET_GLEAM_m[[-c(1,nlyr(PET_GLEAM_m))]])
 
