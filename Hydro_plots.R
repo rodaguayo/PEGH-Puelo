5625a8e5d277441a00930f1f9b97d6189243eebf
Rodrigo Aguayo <roodaguayo@gmail.com>
2022-05-20 00:02:09 +0300

    adjust text position

--- Hydro_plots.R
+++ Hydro_plots.R
@@ -22,6 +22,9 @@ q_sim<-as.data.frame(aggregate(q_sim, as.yearmon(time(q_sim)), mean))
 q_obs<-as.data.frame(aggregate(q_obs, as.yearmon(time(q_obs)), mean))
 period<-as.Date(paste0(rownames(q_sim),1), "%b %Y%d")
 
+#write.csv(q_obs, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Results/Q_obs_monthly.csv")
+#write.csv(q_sim, "C:/Users/rooda/Dropbox/Proyectos/Puelo PEGH/Results/Q_sim_monthly.csv")
+
 q_sim_nc<-q_sim
 q_sim_nc[is.na(q_obs)]<-NA
 data_sim_mean<-as.data.frame(t(monthlyfunction(cbind(period,q_sim), FUN = mean, na.rm = T)))
@@ -42,10 +45,11 @@ for(i in 1:10) {
   
   x1 <- list(titlefont = f, tickfont = f2, ticks = "outside", type = 'date', tickformat = "%Y")
   y1 <- list(titlefont = f, title = "Caudal (m3/s)", tickfont = f2, ticks = "outside", zeroline = FALSE)
-  title1 <-list(text = names_gauge[i], font = f, showarrow = F, xref = "paper", yref = "paper", x = 1, y = 1)
-  title11 <-list(text = "a) Caudal medio anual", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.05, y = 0.05)
+  title1 <-list(text = names_gauge[i], font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.03, y = 1)
+  title11 <-list(text = "a) Caudal medio anual", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.03, y = 0.1)
   fig1 <- plot_ly(showlegend = F, y = data_sim_annual[,i], x = seq(1980,2020), type = 'scatter', mode = 'lines', name = "Sim",  opacity  = 1, line = list(color = "grey", width = 1))
   fig1 <- fig1 %>% add_trace(y = data_obs_annual[,i], mode = 'lines', name = "Obs",  opacity  = 1, line = list(color = "black", width = 1))
+  fig1 <- fig1 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
   fig1 <- fig1 %>% layout(xaxis = x1, yaxis = y1, annotations = title1)
   fig1 <- fig1 %>% layout(annotations = title11)
   
@@ -54,20 +58,23 @@ for(i in 1:10) {
   fig2 <- plot_ly(data_sim_mean, y = data_sim_mean[,i], showlegend = FALSE, x = ~month, type = 'scatter', mode = 'lines+markers', name = "GR4J",  marker = list(size = 3, color = "grey"), line = list(color = "grey", width = 1))
   fig2 <- fig2 %>% add_trace(data_obs_mean, y = data_obs_mean[,i], x = ~month, mode = 'lines+markers', name = "GR5J", marker = list(size = 3, color = "black"), line = list(color = "black", width = 1))
   fig2 <- fig2 %>% layout(xaxis = x2, annotations = title2, showlegend = FALSE)
+  fig2 <- fig2 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
   
   x3 <- list(titlefont = f, tickfont = f2, ticks = "outside", type = 'date', tickformat = "%Y")
   y3 <- list(titlefont = f, title = "Caudal (m3/s)", tickfont = f2, ticks = "outside", zeroline = FALSE)
-  title3 <-list(text = "c) Caudal mensual", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.05, y = 0.9)
+  title3 <-list(text = "c) Caudal mensual", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.03, y = 0.9)
   fig3 <- plot_ly(showlegend = F, y = q_sim[,i], x = period, type = 'scatter', mode = 'lines', name = "Sim",  opacity  = 1, line = list(color = "grey", width = 1))
   fig3 <- fig3 %>% add_trace(y = q_obs[,i], mode = 'lines', name = "Obs",  opacity  = 1, line = list(color = "black", width = 1))
   fig3 <- fig3 %>% layout(xaxis = x3, yaxis = y3, annotations = title3)
+  fig3 <- fig3 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
   fig3
   
   x4 <- list(title = "Probabilidad (%)", titlefont = f, tickfont = f2, ticks = "outside")
-  title4 <-list(text = "d) Curva de duración", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.25, y = 0.9)
+  title4 <-list(text = "d) Curva de duración", font = f, showarrow = F, xref = "paper", yref = "paper", x = 0.22, y = 0.9)
   fig4 <- plot_ly(showlegend = FALSE, y = sort(q_sim_nc[,i]), x =  sort(q_sim_nc_prob[,i])*100, type = 'scatter', mode = 'lines', line = list(size = 3, color = "grey"))
   fig4 <- fig4 %>% add_trace(y = sort(q_obs[,i]), x =  sort(q_obs_prob[,i])*100, mode = 'lines', line = list(size = 3, color = "black"))
   fig4 <- fig4 %>% layout(xaxis = x4, annotations = title4)
+  fig4 <- fig4 %>% layout(plot_bgcolor="rgb(235, 235, 235)")
   
   s1  <- subplot(fig1, fig2, nrows = 1, shareX = T, shareY= T, titleY = T, titleX = T, margin = c(0.04, 0.04, 0.04, 0.04))
   s2  <- subplot(fig3, fig4, nrows = 1, shareX = F, shareY= T, titleY = T, titleX = T, margin = c(0.04, 0.04, 0.04, 0.04))
